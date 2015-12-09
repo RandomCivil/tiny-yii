948b27edb919c9abcf8e760d97a577262eb1a846
RandomCivil <2357243383@qq.com>
2015-12-09 11:29:08 +0800

    del

--- example/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$app = "app";
-define ( "APP", "/youku/" );
-$yii = dirname ( __FILE__ ) . '/framework/yii.php';
-$config = dirname ( __FILE__ ) . '/app/config/main.php';
-require_once ($yii);
-Yii::createWebApplication ($config)->run ();
-?>
